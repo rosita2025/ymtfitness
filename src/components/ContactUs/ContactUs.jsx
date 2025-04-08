477a94fb145e4dc096ebb7936935e4be9cd1d097
rosita2025 <196705675+rosita2025@users.noreply.github.com>
2025-04-08 23:59:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/ContactUs/ContactUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ContactUs.css';
+
+const ContactUs = () => {
+    return (
+        <div className="contact-us-container">
+            <h2>Contact Us</h2>
+            <p>Email: <a href="mailto:contact@neevaa.co.uk">contact@ymtfitness.com</a></p>
+            <p>Phone: <a href="tel:+51955458759">+51955458759</a></p>
+        </div>
+    );
+};
+
+export default ContactUs;
